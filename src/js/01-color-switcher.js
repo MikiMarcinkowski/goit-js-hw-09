e1d14e0430fc237690e14cdfff7394584b73ba91
mikus151 <mikus151@wp.pl>
2023-03-28 23:30:13 +0200

    final -v of task -1 (I think)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,3 @@
-// Napisz skrypt, który po kliknięciu przycisku «Start», raz na sekundę zmienia kolor tła <body> na wartość losową używając do tego stylu inline. Po kliknięciu przycisku «Stop», kolor tła powinien przestać się zmieniać i “zatrzymać” się na aktualnym kolorze.
-
-// UWAGA
-// Zwróć uwagę na to, że przycisk «Start» można klikać w nieskończoność. Zrób tak, żeby przycisk «Start» był nieaktywny, tak długo jak zmiana kolorów jest uruchomiona (użytj atrybutu disabled).
-
-// Aby wygenerować losowy kolor użyj funkcji getRandomHexColor.
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
 
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
